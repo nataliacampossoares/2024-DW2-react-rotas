1ce4c2cbdd36ff7a98a1cf96ff5159b64f973d0f
Natália Campos Soares <130059334+nataliacampossoares@users.noreply.github.com>
2024-10-04 11:56:48 -0300

    Update UseEffect.jsx

--- src/pages/Hooks-básicos/UseEffect.jsx
+++ src/pages/Hooks-básicos/UseEffect.jsx
@@ -1,14 +1,26 @@
+import { useEffect, useState } from "react"
 import { Pagina } from "../../components/Pagina"
 
 export function UseEffect() {
+    
+    const [url, setUrl] = useState("")
+    
+    function trocarImagem() {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resposta => resposta.json()) 
+        .then(objeto => setUrl(objeto.message)) 
+    }
 
-    fetch("https://dog.ceo/api/breeds/image/random")
-    .then(resposta => resposta.json()) //devolve o conteudo da funcao json
-    .then(objeto => console.log(objeto.message)) //conteudo que o json devolve
+    useEffect(trocarImagem, [])
 
     return (
         <Pagina titulo="UseEffect" subtitulo="Hooks Básicos">
-            <img src="https://images.dog.ceo/breeds/saluki/n02091831_6323.jpg" alt="" />
+            <div className="flex flex-col">
+            <img src={url} alt="" />
+            <button className="bg-red-800 px-3 py-2 rounded-md mr-2"
+                    onClick={trocarImagem}
+            >Trocar</button>
+            </div>
         </Pagina>
     )
 }
